5a5effdd9eb11a4a5ed8a01e19137f4749e4a552
Faisal <officialfaisal1112@gmail.com>
2023-10-05 22:38:10 +0600

    fix login form design

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,49 +1,57 @@
 "use client";
 
-import { Button, Col, Row } from "antd";
-import Image from "next/image";
 import loginBanner from "../../assets/login-banner.png";
 import Form from "@/components/Forms/Form";
 import FormInput from "@/components/Forms/FormInput";
 import { SubmitHandler } from "react-hook-form";
+import { Button, Col, Row } from "antd";
+import Image from "next/image";
 
-interface FormValues {
+type FormValues = {
   id: string;
   password: string;
-}
+};
 
 const LoginPage = () => {
   const onSubmit: SubmitHandler<FormValues> = (data) => {
     try {
-      console.log("data: ", data);
-    } catch (error) {}
+      console.log(data);
+    } catch (err) {}
   };
-
   return (
-    <Row>
-      <Col sm={12} md={16} lg={16}>
-        <Image src={loginBanner} alt="login-banner" width={600} />
+    <Row
+      justify="center"
+      align="middle"
+      style={{
+        minHeight: "100vh",
+      }}
+    >
+      <Col sm={12} md={16} lg={10}>
+        <Image src={loginBanner} width={500} alt="login image" />
       </Col>
       <Col sm={12} md={8} lg={8}>
-        <h1>First Login Your Account</h1>
+        <h1
+          style={{
+            margin: "15px 0px",
+          }}
+        >
+          First login your account
+        </h1>
         <div>
           <Form submitHandler={onSubmit}>
             <div>
-              <FormInput
-                label="ID"
-                name="id"
-                type="text"
-                size="large"
-                placeholder="Your ID"
-              />
+              <FormInput name="id" type="text" size="large" label="User Id" />
             </div>
-            <div>
+            <div
+              style={{
+                margin: "15px 0px",
+              }}
+            >
               <FormInput
-                label="Password"
                 name="password"
                 type="password"
                 size="large"
-                placeholder="Your Password"
+                label="User Password"
               />
             </div>
             <Button type="primary" htmlType="submit">
